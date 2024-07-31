bc89bece3836ccc122f712017b21a01a1823a640
JavierMancho <javimanchom@gmail.com>
2024-07-31 19:31:25 +0200

    Uploaded backend and frontend

---
+++ Server/HPEBack/src/main/java/com/HPEBack/HPEBack/HpeBackApplication.java
@@ -0,0 +1,13 @@
+package com.HPEBack.HPEBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpeBackApplication.class, args);
+	}
+
+}
---
+++ Server/HPEBack/src/test/java/com/HPEBack/HPEBack/HpeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HPEBack.HPEBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b2e0e765ba4d0f6e9c4006342c0897decd210fe2
JavierMancho <javimanchom@gmail.com>
2024-08-20 13:05:32 +0200

    Created Script connected to the database to store pdf chunks

--- BOE-Script/script.py
+++ BOE-Script/script.py
@@ -2,25 +2,56 @@ import requests
 import os
 import json
 from datetime import datetime, timedelta
+from flask import Flask, request, jsonify
+import pdfplumber
 
-# Configurar la carpeta de destino principal
+# Create flask app
+app = Flask("BOE-Script")
+
+
+# Configure path to save PDFs
 output_folder = "pdfs"
 os.makedirs(output_folder, exist_ok=True)
 
-# Función para descargar y guardar PDFs
+# Obtain actual date
+global ACTUAL_DATE 
+ACTUAL_DATE = datetime.now()  # Keep ACTUAL_DATE as a datetime object
+
+global LAST_DATE
+# Obtain last date from config.json
+with open('config.json') as json_file:
+    data = json.load(json_file)
+    LAST_DATE = data['last_pdf_date']
+    LAST_DATE = datetime.strptime(LAST_DATE, "%Y%m%d")
+
+
+# Function to save PDFs
 def download_pdf(pdf_url, date_str, unique_id):
     response = requests.get(pdf_url)
     if response.status_code == 200:
         date_folder = os.path.join(output_folder, date_str)
         os.makedirs(date_folder, exist_ok=True)
         pdf_filename = os.path.join(date_folder, f"{unique_id}.pdf")
+        
         with open(pdf_filename, 'wb') as pdf_file:
-            pdf_file.write(response.content)
+            pdf_file.write(response.content)         
+
+        texto_pdf = ''
+        with pdfplumber.open(pdf_filename) as pdf:
+            for pagina in pdf.pages:
+                texto_pdf += pagina.extract_text() or ''  # Añade texto de cada página
+        body = {
+            "date": date_str,
+            "id": unique_id,
+            "content": texto_pdf
+        }
+        response = requests.post("http://localhost:5000/store", json=body)
+
         print(f"PDF guardado: {pdf_filename}")
     else:
         print(f"Error al descargar el PDF {pdf_url}: {response.status_code}")
 
-# Función para buscar recursivamente en el JSON todas las URLs de PDFs
+# Recursive function to find PDFs in JSON data
 def find_pdfs_in_json(data, date_str):
     if isinstance(data, dict):
         for key, value in data.items():
@@ -35,11 +66,11 @@ def find_pdfs_in_json(data, date_str):
         for item in data:
             find_pdfs_in_json(item, date_str)
 
-# Función para obtener y guardar los PDFs de una fecha específica
+# Function to fetch and save PDFs for a given date
 def fetch_and_save_pdfs(date_str):
     url = f"https://boe.es/datosabiertos/api/boe/sumario/{date_str}"
     headers = {
-        'Accept': 'application/json'  # Añadir la cabecera Accept
+        'Accept': 'application/json' 
     }
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
@@ -49,16 +80,33 @@ def fetch_and_save_pdfs(date_str):
         print(f"Error fetching data for {date_str}: {response.status_code}")
         print(response.text)
 
-# Fecha inicial y final para el año 2024
-start_date = datetime.strptime("20240101", "%Y%m%d")
-end_date = datetime.strptime("20240701", "%Y%m%d")
+# Save PDFs into the database
 
-# Iterar sobre cada día del año 2024
-current_date = start_date
-while current_date <= end_date:
-    date_str = current_date.strftime("%Y%m%d")
-    print(f"Fetching data for {date_str}")
-    fetch_and_save_pdfs(date_str)
-    current_date += timedelta(days=1)
 
-print("Descarga completa.")
+@app.route("/last-date", methods=["GET"])
+def get_last_date():
+    return {"last_date": LAST_DATE.strftime("%Y%m%d")}  # Convert to string for response
+
+
+# /update-pdf endpoint to update PDFs
+@app.route("/update-pdf", methods=["GET"])
+def update_pdf():
+    global LAST_DATE
+    global ACTUAL_DATE
+
+    while LAST_DATE <= ACTUAL_DATE:
+        fetch_and_save_pdfs(LAST_DATE.strftime("%Y%m%d"))
+        LAST_DATE += timedelta(days=1)
+    
+    with open('config.json', 'w') as json_file:
+        data = {
+            "last_pdf_date": LAST_DATE.strftime("%Y%m%d")  # Save as string in the correct format
+        }
+        json.dump(data, json_file)
+
+    return {"message": "Download Complete"}
+
+
+# Main function
+if __name__ == "__main__":
+    app.run(host= "0.0.0.0", port=5002, debug=True)
